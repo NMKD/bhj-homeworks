daf84da04e2cefe375dee7568d3a5d6215cbbd57
kristina <kistina@localhost>
2020-08-07 19:54:13 +0300

    реализация remove and add class modal_active done

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,12 +1,24 @@
 const modalMain = document.getElementById('modal_main');
 const modalSucces = document.getElementById('modal_success');
-const modalClose = document.querySelector('.modal__close');
+const modalClose = Array.from(document.querySelectorAll('.modal__close'));
+console.log(modalClose.length);
 const showSuccess = document.querySelector('.show-success');
 
 
-modalClose.onclick = function() {
-    modalMain.classList.remove('modal_active');
+for (let close of modalClose) {
+    close.onclick = function () {
+        console.log(close)
+        if (modalMain.classList.contains('modal_active')) {
+            modalMain.classList.remove('modal_active');
+        } 
+        if (modalSucces.classList.contains('modal_active')) {
+            modalSucces.classList.remove('modal_active');
+        }
+    }
 }
+
+
+
 
 showSuccess.onclick = function(){
     modalMain.classList.remove('modal_active');

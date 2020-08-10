83431d1f5e0c4c667be2049044ce61d837dcc15a
kristina <kistina@localhost>
2020-08-10 22:39:10 +0300

    реализация карусели

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -4,21 +4,43 @@ const slides = document.getElementsByClassName('slider__item');
 const prev = document.querySelector('.slider__arrow_prev');
 const next = document.querySelector('.slider__arrow_next');
 const arrSlides = Array.from(slides);
+let indexSlide = 0;
 
-arrSlides.forEach(function(item, i, arr) {
-    i = 0;
-    // console.log(i, item, arr)
-    next.onclick = function(){
-        if (item == true) {i++}
-        else {item = arr.length}
-        // console.log(i++)
+const cliker = function (prev, next) {
+    //лево
+    prev.addEventListener('click', function(){
+      //индекс картинки
+      const page = valueIndex(arrSlides);
+      indexSlide += page;
+      page.classList.add('slider__item_active');
+      console.log(page.classList.add('slider__item_active'))
+    });
+    //право
+    next.addEventListener('click', function(){
+        //индекс картинки
+        const page = valueIndex(arrSlides);
+        indexSlide -= page;
+        page.classList.add('slider__item_active');
+        console.log(page.classList.add('slider__item_active'))
+      });
+    
+}
+
+const valueIndex = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let slideIndex = arr[i];
+        slideIndex.classList.remove('slider__item_active');
+
+        if (i > 0) {
+            arr[i] = 0;
+        } else if (i < 0) {
+            arr[i] = arr.length;
+        }
+
+        return slideIndex;
     }
-});
-//я так понимаю нужно использовать метод Lisner чтобы можно было бы подключить два метода onclick?
-// + как реализовать чтобы синхронно двигались картинки с индектом?
+    
+}
+ cliker(prev, next);
 
-// prev.onclick = function(){
-//     if (item == true) {i--}
-//     else {item = arr.length}
-//     console.log(i--)
-// }
+

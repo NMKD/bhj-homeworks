99f4e5cc5c3ce8fded5d4939cbca7b87e77e9e0d
kristina <kistina@localhost>
2020-08-12 00:14:44 +0300

    реализация findIndex

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -4,43 +4,26 @@ const slides = document.getElementsByClassName('slider__item');
 const prev = document.querySelector('.slider__arrow_prev');
 const next = document.querySelector('.slider__arrow_next');
 const arrSlides = Array.from(slides);
-let indexSlide = 0;
 
+
+// использовала тенарный знак, без циклов и услови1, переключает только 1 слайдер
 const cliker = function (prev, next) {
     //лево
     prev.addEventListener('click', function(){
-      //индекс картинки
-      const page = valueIndex(arrSlides);
-      indexSlide += page;
-      page.classList.add('slider__item_active');
-      console.log(page.classList.add('slider__item_active'))
+        arrSlides.findIndex((item, index) => index == 'slider__item_active' ? item.classList.remove('slider__item_active') : item.classList.add('slider__item_active'));
+
+  
     });
     //право
     next.addEventListener('click', function(){
-        //индекс картинки
-        const page = valueIndex(arrSlides);
-        indexSlide -= page;
-        page.classList.add('slider__item_active');
-        console.log(page.classList.add('slider__item_active'))
-      });
+        arrSlides.findIndex((item, index) => index === 'slider__item_active' ? item.classList.remove('slider__item_active') : item.classList.add('slider__item_active'));
+            
+    });
     
 }
 
-const valueIndex = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-        let slideIndex = arr[i];
-        slideIndex.classList.remove('slider__item_active');
-
-        if (i > 0) {
-            arr[i] = 0;
-        } else if (i < 0) {
-            arr[i] = arr.length;
-        }
-
-        return slideIndex;
-    }
-    
-}
  cliker(prev, next);
+
+
 
 

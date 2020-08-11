cbc633e6a70d7289334ad1096f64d79ccc43cdd9
kristina <kistina@localhost>
2020-08-11 22:24:37 +0300

    реализация переключение текста класса

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,26 +1,89 @@
+'use strict';
 const tabs = Array.from(document.querySelectorAll('.tab'));
+const tabContent = document.querySelector('.tab__contents');
+const tabNavigator = document.querySelector('.tab__navigation');
 // console.log(tabs);
 const tabsContent = Array.from(document.querySelectorAll('.tab__content'));
 // console.log(tabsContent);
-function clickTab () {
-   for (let activeTab of tabs) {
-        activeTab.addEventListener('click', () => {
-          if (!activeTab.classList.contains('tab_active')) {
-              activeTab.classList.add('tab_active');
-              console.log(activeTab);
-           } if (activeTab.classList.contains('tab_active')) {
-              for (let tabContent of tabsContent) {
-                 tabContent.classList.add('tab__content_active');
-                 console.log(tabContent);
-               }
-            }  
-        });
-   }
+function removeClassOfTabs (arr, tabsActive) {
+   arr.forEach(element => {
+       if (tabsActive === element.closest('div.tab__navigation')) {
+          element.classList.remove('tab_active');
+       }
+   });
+}
 
-   
+//не получается изменить класс текста, как это сделать...
+
+function changeClassOfContent (arr, tabsActive) {
+   arr.forEach(element => {
+       if (tabsActive === element.closest('tab__content')) {
+           element.classList.remove('tab__content_active');
+       } else if (!element.classList.contains('tab__content_active')) {
+           element.classList.add('tab__content_active');
+       }
+   });
 }
 
 
-clickTab ();
+tabs.forEach(function(element) {
+   element.addEventListener('click', function () {
+      if (element.classList.contains('tab_active')) {
+         removeClassOfTabs(tabs, element.closest('div.tab__navigation'));
+         changeClassOfContent(tabsContent, element.closest('tab__content'));
+      } else  {
+         removeClassOfTabs(tabs, element.closest('div.tab__navigation'));
+         changeClassOfContent(tabsContent, element.closest('tab__content'));
+         element.classList.add('tab_active');
+      }
+   });
+});
 
-//не могу понять как реализовать удаление предыдущего класса/если использовать previousElementSibling то он удаляет вкладки..((()))
+
+
+// function addClassOfTabs (arr) {
+//    arr.forEach(element => {
+//        if (!element.classList.contains('tab_active')) {
+//           element.classList.add('tab_active');
+//        }
+//    });
+// }
+
+//function removeClassOfTabs (arr, tabsActive) {
+//    arr.forEach(element => {
+//       if (tabsActive === element.closest('div.tab__navigation') && tabsActive === element.closest('div.tab__contents"')) {
+//          element.classList.remove('tab_active') && element.classList.remove('tab__content_active');
+//       }
+//   });
+  
+// }
+
+// tabs.forEach(function(elem, ind) {
+//   elem.previousElementSibling.onclick = function () {
+//      if (elem.classList.contains('tab_active') && tabsContent[ind].classList.contains('tab__content_active')) {
+//         removeClassOfTabs(tabs, elem.closest('div.tab__navigation')) && removeClassOfTabs(tabsContent, elem.closest('div.tab__contents'));
+//         return;
+//      } else {
+
+//      }
+//   }
+// });
+
+
+// function closeAllMenus(array, mainMenu) {
+//    array.forEach(function (menu) {
+//        (mainMenu === menu.closest('ul.menu_main')) ? menu.classList.remove('menu_active') : '';
+//        });
+// };
+// subMenu.forEach(function (menu) {
+//    menu.previousElementSibling.onclick = function () {
+//        if (menu.classList.contains('menu_active')) {
+//            closeAllMenus(subMenu, menu.closest('ul.menu_main'));
+//            return false;
+//        } else {
+//            closeAllMenus(subMenu, menu.closest('ul.menu_main'));
+//            menu.classList.add('menu_active');
+//            return false;
+//        };
+//    };
+
